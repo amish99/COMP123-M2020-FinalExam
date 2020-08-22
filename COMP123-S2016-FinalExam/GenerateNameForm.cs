a66b2d3291f1c7298a30cafdf4a3eb58deb5b10e
amish tangri <amishtangri123@gmail.com>
2020-08-21 22:53:46 -0400

    Add project files.

---
+++ COMP123-S2016-FinalExam/GenerateNameForm.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COMP123_M2020_FinalExam
+{
+    /*
+* Name: amish tangri
+* Student ID: 301108446
+* Date: August 21, 2020
+*/
+
+    public partial class GenerateNameForm : Form
+    {
+        public void GenerateNames()
+        {
+            //generating random names in the textboxes from the list
+            var random = new Random();
+            int indexfirst = random.Next(0, FirstNameListBox.Items.Count);
+            firstNameTextBox.Text = FirstNameListBox.Items[indexfirst].ToString();
+
+            int indexlast = random.Next(0, lastNameListBox.Items.Count);
+            LastNameTextBox.Text = lastNameListBox.Items[indexlast].ToString();
+        }
+        public GenerateNameForm()
+        {
+            InitializeComponent();
+        }
+
+        // when generate name form loads
+        private void GenerateNameForm_Load(object sender, EventArgs e)
+        {
+            //generate name method is called
+            GenerateNames();
+        }
+
+        // when generate name button is clicked
+        private void GenerateButton_Click(object sender, EventArgs e)
+        {
+            //generate name method is called
+            GenerateNames();
+        }
+
+        private void NextButton_Click(object sender, EventArgs e)
+        {
+            //the following sets the value of the properties to the value that was generated for the textfields
+            Program.character.FirstName = firstNameTextBox.Text;
+            Program.character.LastName = LastNameTextBox.Text;
+
+            // instantiated abilitygeneratorform object
+            AbilityGeneratorForm abilityGeneratorForm = new AbilityGeneratorForm();
+
+            //showing abilitygenerator form
+            abilityGeneratorForm.Show();
+
+            //hiding generate name form
+            Program.generatenameform.Hide();
+        }
+    }
+}
---
+++ COMP123-S2016-FinalExam/SplashForm.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COMP123_M2020_FinalExam
+{
+    public partial class SplashForm : Form
+    {
+        public SplashForm()
+        {
+            InitializeComponent();
+        }
+
+        // this will hide the splash form screen and then it will show generate form
+        
+        private void splashformtimer_Tick(object sender, EventArgs e)
+        {
+            Program.generatenameform.Show();
+            this.Hide();
+            splashformtimer.Enabled = false;
+        }
+    }
+}
